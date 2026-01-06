a0d1a29da5341e2ada81302a434c5171d7c42650
Carlos Garcés <carlos.garces@gmail.com>
2026-01-06 13:44:16 +0100

    Endopints de comunicades autonomas y provincias No son propiamente parte de los datos del INE pero se necesitan para la demo.

--- api_rest/app/main.py
+++ api_rest/app/main.py
@@ -8,6 +8,144 @@ con = duckdb.connect("callejero.duckdb", config={"access_mode": "READ_ONLY"})
 end = time.time()
 print(f"Loaded TRAM table in {end - start:.2f} seconds")
 
+dict_auto = {
+    "01": "ANDALUCIA",
+    "02": "ARAGON",
+    "03": "ASTURIAS",
+    "04": "BALEARES",
+    "05": "CANARIAS",
+    "06": "CANTABRIA",
+    "07": "CASTILLA Y LEON",
+    "08": "CASTILLA LA MANCHA",
+    "09": "CATALUNA",
+    "10": "COMUNITAT VALENCIANA",
+    "11": "EXTREMADURA",
+    "12": "GALICIA",
+    "13": "MADRID",
+    "14": "MURCIA",
+    "15": "NAVARRA",
+    "16": "PAIS VASCO",
+    "17": "RIOJA, LA",
+    "18": "CEUTA",
+    "19": "MELILLA",
+}
+
+dict_provincia = {
+    "01": {"PRO": "ALAVA", "CCOM": "16"},
+    "02": {"PRO": "ALBACETE", "CCOM": "08"},
+    "03": {"PRO": "ALICANTE/ALACANT", "CCOM": "10"},
+    "04": {"PRO": "ALMERIA", "CCOM": "01"},
+    "05": {"PRO": "AVILA", "CCOM": "07"},
+    "06": {"PRO": "BADAJOZ", "CCOM": "11"},
+    "07": {"PRO": "BALEARS, ILLES", "CCOM": "04"},
+    "08": {"PRO": "BARCELONA", "CCOM": "09"},
+    "09": {"PRO": "BURGOS", "CCOM": "07"},
+    "10": {"PRO": "CACERES", "CCOM": "11"},
+    "11": {"PRO": "CADIZ", "CCOM": "01"},
+    "12": {"PRO": "CASTELLON/CASTELLO", "CCOM": "10"},
+    "13": {"PRO": "CIUDAD REAL", "CCOM": "08"},
+    "14": {"PRO": "CORDOBA", "CCOM": "01"},
+    "15": {"PRO": "CORUNA, A", "CCOM": "12"},
+    "16": {"PRO": "CUENCA", "CCOM": "08"},
+    "17": {"PRO": "GIRONA", "CCOM": "09"},
+    "18": {"PRO": "GRANADA", "CCOM": "01"},
+    "19": {"PRO": "GUADALAJARA", "CCOM": "08"},
+    "20": {"PRO": "GIPUZKOA", "CCOM": "16"},
+    "21": {"PRO": "HUELVA", "CCOM": "01"},
+    "22": {"PRO": "HUESCA", "CCOM": "02"},
+    "23": {"PRO": "JAEN", "CCOM": "01"},
+    "24": {"PRO": "LEON", "CCOM": "07"},
+    "25": {"PRO": "LLEIDA", "CCOM": "09"},
+    "26": {"PRO": "RIOJA, LA", "CCOM": "17"},
+    "27": {"PRO": "LUGO", "CCOM": "12"},
+    "28": {"PRO": "MADRID", "CCOM": "13"},
+    "29": {"PRO": "MALAGA", "CCOM": "01"},
+    "30": {"PRO": "MURCIA", "CCOM": "14"},
+    "31": {"PRO": "NAVARRA", "CCOM": "15"},
+    "32": {"PRO": "OURENSE", "CCOM": "12"},
+    "33": {"PRO": "ASTURIAS", "CCOM": "03"},
+    "34": {"PRO": "PALENCIA", "CCOM": "07"},
+    "35": {"PRO": "PALMAS, LAS", "CCOM": "05"},
+    "36": {"PRO": "PONTEVEDRA", "CCOM": "12"},
+    "37": {"PRO": "SALAMANCA", "CCOM": "07"},
+    "38": {"PRO": "SANTA CRUZ DE TENERIFE", "CCOM": "05"},
+    "39": {"PRO": "CANTABRIA", "CCOM": "06"},
+    "40": {"PRO": "SEGOVIA", "CCOM": "07"},
+    "41": {"PRO": "SEVILLA", "CCOM": "01"},
+    "42": {"PRO": "SORIA", "CCOM": "07"},
+    "43": {"PRO": "TARRAGONA", "CCOM": "09"},
+    "44": {"PRO": "TERUEL", "CCOM": "02"},
+    "45": {"PRO": "TOLEDO", "CCOM": "08"},
+    "46": {"PRO": "VALENCIA/VALENCIA", "CCOM": "10"},
+    "47": {"PRO": "VALLADOLID", "CCOM": "07"},
+    "48": {"PRO": "BIZKAIA", "CCOM": "16"},
+    "49": {"PRO": "ZAMORA", "CCOM": "07"},
+    "50": {"PRO": "ZARAGOZA", "CCOM": "02"},
+    "51": {"PRO": "CEUTA", "CCOM": "18"},
+    "52": {"PRO": "MELILLA", "CCOM": "19"},
+}
+
+
+@app.get(
+    "/autonomias/",
+    summary="Listado de todas las comunidades autonomas",
+    responses={
+        200: {"description": "Listado de autonomias"},
+    },
+)
+def get_autonomias():
+    """Devuelve el listado de comunidades autonomas con su codigo y nombre."""
+    items = [{"CCOM": code, "AUTO": name} for code, name in dict_auto.items()]
+    return items
+
+
+@app.get(
+    "/provincias/",
+    summary="Listado de todas las provincias",
+    responses={
+        200: {"description": "Listado de provincias"},
+        404: {"description": "No se encontro la comunidad autonoma"},
+    },
+)
+def get_provincias():
+    """Devuelve el listado de provincias con su codigo, nombre y comunidad autonoma."""
+    items = [
+        {
+            "CODPRO": code,
+            "PRO": info["PRO"],
+            "CCOM": info["CCOM"],
+            "AUTO": dict_auto[info["CCOM"]],
+        }
+        for code, info in dict_provincia.items()
+    ]
+    if len(items) == 0:
+        raise HTTPException(
+            status_code=404, detail="Sin resultados para esa comunidad autonoma"
+        )
+    return items
+
+
+@app.get(
+    "/provincias/{ccom}",
+    summary="Listado de todas las provincias de una comunidad autonoma",
+    responses={
+        200: {"description": "Listado de provincias"},
+    },
+)
+def get_provincias_by_ccom(ccom: int):
+    """Devuelve el listado de provincias de una comunidad autonom con su codigo, nombre y comunidad autonoma."""
+    items = [
+        {
+            "CODPRO": code,
+            "PRO": info["PRO"],
+            "CCOM": info["CCOM"],
+            "AUTO": dict_auto[info["CCOM"]],
+        }
+        for code, info in dict_provincia.items()
+        if int(info["CCOM"]) == ccom
+    ]
+    return items
+
 
 @app.get(
     "/cp/{cpos}",
@@ -24,6 +162,7 @@ def get_localidades_by_cp(cpos: str):
     Devuelve el codigo de provincia, municipio y descripcion para un codigo postal (cpos).
     Permite busquedas parciales con un minimo de 3 caracteres
     """
+    # El cp se envia intencianadamente como str para mantener los ceros a la izquierda
     if not cpos.isdigit():
         raise HTTPException(status_code=400, detail="cpos debe ser numérico")
 
